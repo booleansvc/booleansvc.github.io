573da062d8a26b4951b5d0ac422b6cb7066697d2
abhinrustagi <rustagi.abhin@gmail.com>
2020-09-19 19:38:20 +0530

    Add Scroll Reveal

--- js/partials.js
+++ js/partials.js
@@ -5,3 +5,19 @@ $(window).scroll(function() {
         $('#nav').removeClass('nav-active');
     }
 });
+
+// for (var i=0; i < document.body.childNodes.length)
+const nav = document.getElementById("nav");
+ScrollReveal().reveal(nav, {
+  origin: "top",
+  distance: "10px",
+  delay: "100"
+})
+const sections = document.getElementsByTagName("section");
+for (i=0; i<sections.length; i++) {
+  ScrollReveal().reveal(sections[i], {
+    origin: 'bottom',
+    distance: '10px',
+    delay: "200"
+  });
+}
